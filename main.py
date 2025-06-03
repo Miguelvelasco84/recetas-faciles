aecd5f20afec635f12e3c82a0c053399ea72971b
Miguelvelasco84 <124646545+Miguelvelasco84@users.noreply.github.com>
2025-06-02 21:05:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ app.add_middleware(
         "http://localhost:8080",
         "http://127.0.0.1:8080",
         "https://localhost:3000"
+         "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
